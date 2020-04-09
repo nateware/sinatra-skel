1cb33e090232cd2556a4cc00a0556befc856e9c4
Nate Wiger <nwiger@gmail.com>
2020-04-08 20:41:53 -0700

    yay

--- app/routes/movies.rb
+++ app/routes/movies.rb
@@ -2,6 +2,7 @@ class App
   namespace '/movies' do
     get do
       data = {
+        title: 'Movies',
         movies: [
           {'name' => "Star Wars", 'price' => '10.99'},
           {'name' => "Star Trek", 'price' => '14.99'}
